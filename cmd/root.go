c6d871c8db271dbfdee73f74d8eeff04ed7030bc
Yusaku Mizobuchi <yusakum1311@gmail.com>
2026-03-17 00:46:12 +0900

    style: fix formatting in cmd/root.go

--- cmd/root.go
+++ cmd/root.go
@@ -25,7 +25,7 @@ func Execute() error {
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&outputFormat, "format", "f", "json", "Output format (json, table)")
 
-	rootCmd.AddCommand(		newLoginCmd(),
+	rootCmd.AddCommand(newLoginCmd(),
 		newLogoutCmd(),
 		newStatusCmd(),
 		newConfigCmd(),
